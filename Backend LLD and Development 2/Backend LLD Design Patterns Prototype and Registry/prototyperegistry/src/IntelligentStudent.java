775ad79a6cf0c2fd061bc2ab83469520ceee7cdf
Sanyam Lawania <39163842+Sylar009@users.noreply.github.com>
2024-06-13 16:47:22 +0530

    Backend LLD: Design Patterns: Prototype and Registry

---
+++ Backend LLD and Development 2/Backend LLD Design Patterns Prototype and Registry/prototyperegistry/src/IntelligentStudent.java
@@ -0,0 +1,17 @@
+public class IntelligentStudent extends Student {
+    int iq;
+
+    public IntelligentStudent(){
+
+    }
+    // COPY CONSTRUCTOR.
+    public IntelligentStudent(IntelligentStudent s) {
+        super(s);
+        this.iq = s.iq;
+    }
+
+    @Override
+    public IntelligentStudent clone() {
+        return new IntelligentStudent(this);
+    }
+}
