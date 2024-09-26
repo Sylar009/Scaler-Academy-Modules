6cbfd4ad78e75e8607399e8649254c226d8bfa1e
Sanyam Lawania <39163842+Sylar009@users.noreply.github.com>
2024-09-26 11:42:01 +0530

    Advanced DSA : Hashing 1: Introduction & Problems

---
+++ Advanced DSA 2/Advanced DSA - Hashing 1 - Introduction & Problems/Assignment/Q3. Sub-array with 0 sum.cpp
@@ -0,0 +1,104 @@
+/*
+Problem Description
+
+Given an array of integers A, find and return whether the given array contains a non-empty subarray with a sum equal to 0.
+
+If the given array contains a sub-array with sum zero return 1, else return 0.
+
+
+
+
+Problem Constraints
+
+1 <= |A| <= 100000
+
+-10^9 <= A[i] <= 10^9
+
+
+
+
+Input Format
+
+The only argument given is the integer array A.
+
+
+
+Output Format
+
+Return whether the given array contains a subarray with a sum equal to 0.
+
+
+
+Example Input
+
+Input 1:
+
+
+ A = [1, 2, 3, 4, 5]
+
+
+Input 2:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ A = [4, -1, 1]
+
+
+
+
+
+
+
+
+
+
+
+Example Output
+
+Output 1:
+
+ 0
+Output 2:
+
+ 1
+
+
+Example Explanation
+
+Explanation 1:
+
+ No subarray has sum 0.
+Explanation 2:
+
+ The subarray [-1, 1] has sum 0.
+
+
+*/
+
+int Solution::solve(vector<int> &A) {
+    
+    unordered_set<long> hs;
+    hs.insert(0);
+    long ps = 0;
+    for (int a : A) {
+        ps+= a;
+        if (hs.find(ps) != hs.end()) return 1;
+        else hs.insert(ps);
+    }
+    
+    return 0;
+}
